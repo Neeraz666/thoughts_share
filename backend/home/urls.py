1b57326c5f45ad5206582f7ad29931ecda0ace9a
Niraj Pandey <neeraz.pandey2058@gmail.com>
2024-01-25 17:07:45 +0545

    Created LogoutView.

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, LogoutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name = "HomeView"),
+    path('logout/', LogoutView.as_view(), name = "LogoutView"),
 ]
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework import permissions, status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 class HomeView(APIView):
@@ -11,3 +12,20 @@ class HomeView(APIView):
         content = ({"message": "Welcome to Home page!"})
     
         return Response(content)
+    
+
+class LogoutView(APIView):
+
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    
