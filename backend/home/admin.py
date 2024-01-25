981b5962a01ca8c5ad8373b2cece426c61e7f019
Niraj Pandey <neeraz.pandey2058@gmail.com>
2024-01-25 17:33:33 +0545

    Content Models created with user as a foreign key.

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Content
 
 # Register your models here.
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ("user", "title", "postdate")
+    list_display_links = ("user", "title")
+    search_fields = ("user", "title")
+    list_per_page = 25
+    readonly_fields = ("title", "thoughts")
+
+admin.site.register(Content, ContentAdmin)
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import UserAccount
+from datetime import datetime
 
 # Create your models here.
+class Content(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    thoughts = models.TextField()
+    postdate = models.DateTimeField(default=datetime.now, blank=True)
+
+
+    def __str__(self):
+        return self.title
+    
